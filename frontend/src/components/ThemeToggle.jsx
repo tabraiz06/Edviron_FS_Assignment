ad21d2c4237840a06ad5289338fe84cf2c92a3ce
Md Tabraiz <mohammadtabraiz3@gmail.com>
2024-12-24 22:35:18 +0530

    logout added

--- frontend/src/components/ThemeToggle.jsx
+++ frontend/src/components/ThemeToggle.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ThemeToggle() {
+  
   const [darkMode, setDarkMode] = useState(
     localStorage.getItem("theme") === "dark"
   );
@@ -15,14 +17,20 @@ export default function ThemeToggle() {
     }
   }, [darkMode]);
 
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  }
+
   return (
-    <div className="p-4">
+    <div className="p-4 flex justify-between">
       <button
         onClick={() => [setDarkMode(!darkMode),window.location.reload()]}
         className="p-2 bg-gray-700 dark:bg-gray-700 rounded text-white"
       >
         {darkMode ? "Light Mode" : "Dark Mode"}
       </button>
+      <button className="bg-red-500 p-2 rounded" onClick={handleLogout}>Logout</button>
     </div>
   );
 }
