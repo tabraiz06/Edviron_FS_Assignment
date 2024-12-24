6365b42f458e72d71c70a9aaf7d0eab6ba35478c
Md Tabraiz <mohammadtabraiz3@gmail.com>
2024-12-24 22:09:47 +0530

    update in index.js and db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
+const importCSV = require("../utils/importCSV");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000, // Adjust as needed
-      socketTimeoutMS: 45000, // Adjust as needed
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected`);
+
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
