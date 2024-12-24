9fc9e7cc4796ae83e71cdff530f9c36903c1e073
Md Tabraiz <mohammadtabraiz3@gmail.com>
2024-12-24 22:01:39 +0530

    update in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // Adjust as needed
+      socketTimeoutMS: 45000, // Adjust as needed
+    });
     console.log(`MongoDB Connected`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
--- backend/utils/importCSV.js
+++ backend/utils/importCSV.js
@@ -2,10 +2,9 @@ const csv = require("csv-parser");
 const fs = require("fs");
 const Transaction = require("../models/Transaction");
 
-const importCSV = async(filePath) => {
+const importCSV = async (filePath) => {
   await Transaction.deleteMany(); // Clear existing data
   fs.createReadStream(filePath)
-  
     .pipe(csv())
     .on("data", async (row) => {
       const transaction = new Transaction(row);
